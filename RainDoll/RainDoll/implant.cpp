6896553524d9eba2674cc70f06cbc739f2b243ff
Jared1407 <jdguthri@asu.edu>
2025-08-21 11:58:50 -0700

    Updating Implant

--- RainDoll/RainDoll/implant.cpp
+++ RainDoll/RainDoll/implant.cpp
@@ -1,6 +1,3 @@
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#endif
 
 #include "implant.h"
 #include "tasks.h"
--- RainDoll/RainDoll/main.cpp
+++ RainDoll/RainDoll/main.cpp
@@ -1,6 +1,4 @@
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#endif
+
 
 #include "implant.h"
 
