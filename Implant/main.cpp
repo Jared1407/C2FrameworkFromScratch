e1a7c5ac17db935aa498f73c275db75dfba70a9c
Jared <jdguthri@asu.edu>
2025-08-20 19:13:38 -0700

    Implant init

---
+++ Implant/main.cpp
@@ -0,0 +1,13 @@
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <stdio.h>
+
+int main()
+{
+    // Specify address, port and URI of listening post endpoint
+    const auto host = "localhost";
+    const auto port = "5000";
+    const auto uri = "/results";
+}
