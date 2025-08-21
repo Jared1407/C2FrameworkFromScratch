afc3aa8eedab2b6c8d0c1bb1d0061619275a98a3
Jared1407 <jdguthri@asu.edu>
2025-08-20 22:38:21 -0700

    adding post options to fireworks.py

--- Firework/fireworks.py
+++ Firework/fireworks.py
@@ -12,6 +12,11 @@ def api_get_request(endpoint):
     response_json = json.loads(response_raw)
     return response_json
 
+def api_post_request(endpoint, payload):
+    response_raw = requests.post(listening_post_addr + endpoint, json=payload).text
+    response_json = json.loads(response_raw)
+    return response_json
+
 # CLI commands and logic
 @click.group()
 def cli():
@@ -41,10 +46,52 @@ def list_history():
     pprint.pprint(api_get_request(api_endpoint))
     print()
 
+@click.command(name="add-tasks")
+@click.option('--tasktype', help='Type of task to submit.')
+@click.option('--options', help='Key-value options for task.')
+def add_tasks(tasktype, options):
+    """Submit tasks to the listening post."""
+    api_endpoint = "/tasks"
+    print("\nHere are the tasks that were added:\n")
+
+    # Perform options parsing if user provided them to the task
+    if options != None:
+        task_options_dict = {}
+        task_options_pairs = options.split(",")
+
+        # For each key-value pair, add them to a dictionary
+        for option in task_options_pairs:
+            key_vals = option.split("=")
+            key = key_vals[0]
+            value = key_vals[1]
+            pair = {key:value}
+            task_options_dict.update(pair)
+
+        # If more than one option was provided, format and append them into a single string
+        if len(task_options_dict) > 1:
+            keyval_string = ""
+            for key,value in task_options_dict.items():
+                keyval_string += f'"{key}":"{value}",'
+            request_payload_string = f'[{{"task_type":"{tasktype}",{keyval_string[:-1]}}}]'
+            request_payload = json.loads(request_payload_string)
+            pprint.pprint(api_post_request(api_endpoint, request_payload))
+        # Otherwise, just print the key/value for the single option provided
+        else:
+            request_payload_string = f'[{{"task_type":"{tasktype}","{key}":"{value}"}}]'
+            request_payload = json.loads(request_payload_string)
+            pprint.pprint(api_post_request(api_endpoint, request_payload))
+    # Otherwise, we just submit a payload with the task type specified
+    else:
+        request_payload_string = f'[{{"task_type":"{tasktype}"}}]'
+        request_payload = json.loads(request_payload_string)
+        pprint.pprint(api_post_request(api_endpoint, request_payload))
+    print()
+
 # Add commands to CLI
 cli.add_command(list_tasks)
 cli.add_command(list_results)
 cli.add_command(list_history)
+cli.add_command(add_tasks)
 
 if __name__ == '__main__':
     cli()
