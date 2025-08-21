35845672d32c96901d15b9ecf3dfc961f97fcacc
Jared <jdguthri@asu.edu>
2025-08-20 19:40:50 -0700

    Update main.cpp

--- Implant/main.cpp
+++ Implant/main.cpp
@@ -2,7 +2,9 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+#include "implant.h"
 #include <stdio.h>
+#include <boost/system/system_error.hpp>
 
 int main()
 {
@@ -10,4 +12,7 @@ int main()
     const auto host = "localhost";
     const auto port = "5000";
     const auto uri = "/results";
+
+    // Instantiate our implant object
+    Implant implant{ host, port, uri};
 }
